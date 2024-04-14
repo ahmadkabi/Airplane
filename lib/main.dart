36a81108653daf7ab67e428347f46cda863f5793
ahmadkabi <ahmadkabi.dev@gmail.com>
2024-04-15 06:46:39 +0700

    UI Slicing : Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:airplane/cubit/page_cubit.dart';
 import 'package:airplane/ui/pages/bonus_page.dart';
 import 'package:airplane/ui/pages/detail_page.dart';
 import 'package:airplane/ui/pages/get_started_page.dart';
@@ -5,6 +6,7 @@ import 'package:airplane/ui/pages/main_page.dart';
 import 'package:airplane/ui/pages/sign_up_page.dart';
 import 'package:airplane/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,15 +15,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => const SplashPage(),
-        '/get-started': (context) => const GetStartedPage(),
-        '/sign-up': (context) => const SignUpPage(),
-        '/bonus': (context) => const BonusPage(),
-        '/main': (context) => const MainPage(),
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => PageCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (context) => const SplashPage(),
+          '/get-started': (context) => const GetStartedPage(),
+          '/sign-up': (context) => const SignUpPage(),
+          '/bonus': (context) => const BonusPage(),
+          '/main': (context) => const MainPage(),
+        },
+      ),
     );
   }
 }
--- lib/ui/widgets/custom_bottom_navigation_item.dart
+++ lib/ui/widgets/custom_bottom_navigation_item.dart
@@ -1,36 +1,48 @@
+import 'package:airplane/cubit/page_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../shared/theme.dart';
 
 class CustomBottomNavigationItem extends StatelessWidget {
+  final int index;
   final String imageUrl;
-  final bool isSelected;
 
   const CustomBottomNavigationItem({
     super.key,
+    required this.index,
     required this.imageUrl,
-    this.isSelected = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        const SizedBox(),
-        Container(
-          height: 24,
-          width: 24,
-          decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage(imageUrl))),
-        ),
-        Container(
-          height: 2,
-          width: 30,
-          decoration: BoxDecoration(
-              color: isSelected ? kPrimaryColor : kTransparentColor,
-              borderRadius: BorderRadius.circular(defaultRadius)),
-        ),
-      ],
+    return GestureDetector(
+      onTap: () {
+        context.read<PageCubit>().setPage(index);
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const SizedBox(),
+          Image.asset(
+            imageUrl,
+            height: 24,
+            width: 24,
+            color: context.watch<PageCubit>().state == index
+                ? kPrimaryColor
+                : kGreyColor,
+          ),
+          Container(
+            height: 2,
+            width: 30,
+            decoration: BoxDecoration(
+              color: context.watch<PageCubit>().state == index
+                  ? kPrimaryColor
+                  : kTransparentColor,
+              borderRadius: BorderRadius.circular(defaultRadius),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
