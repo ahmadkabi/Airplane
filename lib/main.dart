27c447580e050c351591f701fd40a9ea20ca7bc9
ahmadkabi <achmad.kabi@ideku.id>
2024-04-21 17:31:52 +0700

    Add Firebase to the project (core)

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,18 @@ import 'package:airplane/ui/pages/sign_up_page.dart';
 import 'package:airplane/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
