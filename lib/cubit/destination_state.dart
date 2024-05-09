571d5095bd9500c37587d646cc36e3c78d0a6b53
ahmadkabi <ahmadkabi.dev@gmail.com>
2024-05-09 17:07:42 +0700

    Build Destinations

---
+++ lib/cubit/destination_state.dart
@@ -0,0 +1,33 @@
+part of 'destination_cubit.dart';
+
+sealed class DestinationState extends Equatable {
+  const DestinationState();
+}
+
+final class DestinationInitial extends DestinationState {
+  @override
+  List<Object> get props => [];
+}
+
+
+class DestinationLoading extends DestinationState {
+  @override
+  List<Object?> get props => [];
+}
+
+class DestinationSuccess extends DestinationState {
+  final List<DestinationModel> destinations;
+
+  DestinationSuccess(this.destinations);
+
+  @override
+  List<Object?> get props => [destinations];
+}
+class DestinationFailed extends DestinationState {
+  final String error;
+
+  DestinationFailed(this.error);
+
+  @override
+  List<Object?> get props => [error];
+}
