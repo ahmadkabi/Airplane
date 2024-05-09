8321b83965bf48ee540772f15c8eb0b4de94a806
ahmadkabi <ahmadkabi.dev@gmail.com>
2024-05-09 17:22:36 +0700

    Build Destinations

--- lib/cubit/destination_cubit.dart
+++ lib/cubit/destination_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:airplane/services/destination_service.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
@@ -7,4 +8,17 @@ part 'destination_state.dart';
 
 class DestinationCubit extends Cubit<DestinationState> {
   DestinationCubit() : super(DestinationInitial());
+
+  void fetchDestinations() async{
+    try{
+      emit(DestinationLoading());
+
+      List<DestinationModel> destinations = await DestinationService().fetchDestinations();
+
+      emit(DestinationSuccess(destinations));
+    }catch(e){
+      emit(DestinationFailed(e.toString()));
+    }
+  }
+
 }
